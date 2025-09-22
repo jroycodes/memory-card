5a847d6abbe4f97add6d0b8f8dac99b472a6972e
Jonathan Roy <jonaisroy@gmail.com>
2025-09-22 16:58:50 +0300

    images component [app workflow]

---
+++ src/Images.jsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import "./styles.css";
+
+export default function Images() {
+  const allImages = [
+    "/images/Aang.jpg",
+    "/images/Appa.jpg",
+    "/images/Azula.jpg",
+    "/images/Bumi.jpg",
+    "/images/Cabbage-man.jpg",
+    "/images/Iroh.jpg",
+    "/images/Jet.jpg",
+    "/images/Katara.jpg",
+    "/images/Mai.jpg",
+    "/images/Momo.jpg",
+    "/images/Ozai.jpg",
+    "/images/Roku.jpg",
+    "/images/Sokka.jpg",
+    "/images/Suki.jpg",
+    "/images/Toph.jpg",
+    "/images/tyLee.jpg",
+    "/images/Yue.jpg",
+    "/images/Zuko.jpg",
+    "/images/Kyoshi.jpg",
+    "/images/Zhao.jpg",
+  ];
+
+  const [images, setImages] = useState(allImages);
+  const [clickedImages, setClickedImages] = useState(new Set());
+  const [currentScore, setCurrentScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+
+  function shuffleArray(arr) {
+    const shuffled = [...arr];
+    for (let i = shuffled.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }
+    return shuffled;
+  }
+
+  function handleClick(image) {
+    if (clickedImages.has(image)) {
+      if (currentScore > bestScore) setBestScore(currentScore);
+      setCurrentScore(0);
+      setClickedImages(new Set());
+    } else {
+      setCurrentScore((prev) => prev + 1);
+      setClickedImages((prev) => new Set(prev).add(image));
+    }
+    setImages(shuffleArray(images));
+  }
+
+  return (
+    <div className="flex flex-col items-center">
+      <div className="p-4 mb-4 w-full text-center">
+        <p>Current Score {currentScore}</p>
+        <p>Best Score {bestScore}</p>
+      </div>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
+        {images.map((src, index) => {
+          const name = src.split("/").pop().replace(".jpg", "");
+          return (
+            <div
+              key={index}
+              className="relative w-24 h-24 sm:w-28 sm:h-28 md:w-32 md:h-32 lg:w-36 lg:h-36 mx-auto bg-center bg-no-repeat bg-contain border rounded cursor-pointer"
+              style={{ backgroundImage: `url(${src})` }}
+              onClick={() => handleClick(src)}
+            >
+              <div className="absolute bottom-0 left-0 right-0 bg-black/60 text-white text-xs text-center py-1 rounded-b">
+                {name}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
